543f23537949c6c3c4fe71994663b0f9711d05ae
cormickwhite <cormickwhite@gmail.com>
2024-08-17 17:30:40 -0500

    add auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,9 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+import { signIn } from "@/auth";
+
+import { AuthError } from "next-auth";
 import { sql } from "@vercel/postgres";
 
 import { z } from "zod";
@@ -100,5 +103,24 @@ export async function deleteInvoice(id: string) {
     return { message: "Deleted Invoice" };
   } catch (e) {
     return { message: `Database Error: Failed to Delete Invoice. ${e}` };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
